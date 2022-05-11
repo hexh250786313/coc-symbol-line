d3ef78222279f1baa96f25e3cf7fd901fd076057
hexh <250786313@qq.com>
2022-05-11 19:59:20 +0800

    perf: do not filter callback symbol

--- src/index.ts
+++ src/index.ts
@@ -45,7 +45,6 @@ class DocumentSymbolLine {
       (s) =>
         s.range &&
         ['Class', 'Method', 'Function', 'Struct', 'Property', 'Variable'].includes(s.kind) &&
-        !s.text.endsWith(') callback') &&
         positionInRange(position, s.range) == 0
     );
 
